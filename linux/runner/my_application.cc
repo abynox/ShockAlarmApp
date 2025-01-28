2a72f8d220e6849ee322ba1eb7de2f63e44732da
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2025-01-28 23:12:45 +0100

    update header

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "shock_alarm_app");
+    gtk_header_bar_set_title(header_bar, "ShockAlarm");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "shock_alarm_app");
+    gtk_window_set_title(window, "ShockAlarm");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
