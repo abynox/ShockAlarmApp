11bf776be0c2feda2aa3e3cd50898a3de4c2b0d1
Abynox <abynox@abynox.de>
2025-03-13 22:45:52 +0100

    Updated flutter to 3.29.2, removed uni_links5, added protocol handler

--- android/app/src/main/kotlin/com/example/shock_alarm_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/shock_alarm_app/MainActivity.kt
@@ -1,5 +1,22 @@
 package de.computerelite.shockalarm
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (intent?.action == Intent.ACTION_VIEW) {
+            flutterEngine?.dartExecutor?.let {
+                MethodChannel(
+                    it,
+                    "shock-alarm/protocol"
+                ).invokeMethod("onProtocolUrlReceived", intent.data?.toString())
+            }
+        }
+    }
+}
