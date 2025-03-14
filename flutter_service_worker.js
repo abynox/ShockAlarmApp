0c8c8df1dcde35165f584ae268487bea82f9d65b
abynox <github@abynox.de>
2025-03-14 06:49:51 +0000

    Deploying to gh-pages from @ abynox/ShockAlarmApp@e953971fe887324a27296c17cf95acb3896f2677 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "2806d119770484b438526f2704bc2685",
 "manifest.json": "0c8213e3b89f788c150e51fda6e7f89b",
 "favicon.png": "a3a82b98e5be892d2affe92ebd2f6a56",
-"flutter_bootstrap.js": "f335abfeec5f655a384e5258fd8b610c"};
+"flutter_bootstrap.js": "b7cf9d9796686ca8a962684971b86988"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
